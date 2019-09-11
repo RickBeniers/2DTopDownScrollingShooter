f266efa5295d8b54a2659a5a6d0ce1ad0960991b
RickBeniers <Beniersrick@gmail.com>
2019-09-11 18:05:30 +0200

    made/added sounds

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField]
+    private bool mcExists;
+    [SerializeField]
+    private AudioSource[] musicTracks;
+    [SerializeField]
+    private int currentTrack;
+    [SerializeField]
+    private bool musicCanPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!mcExists)
+        {
+            mcExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (musicCanPlay)
+        {
+            if (!musicTracks[currentTrack].isPlaying)
+            {
+                musicTracks[currentTrack].Play();
+            }
+        }
+        else
+        {
+            musicTracks[currentTrack].Stop();
+        }
+    }
+}
