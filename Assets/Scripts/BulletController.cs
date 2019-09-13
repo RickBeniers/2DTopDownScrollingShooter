29b8902a6eefe68f0e7dc03c85b6b42856efbb78
RickBeniers <Beniersrick@gmail.com>
2019-09-13 19:29:39 +0200

    added enemy plane sprite and auto shooting when holding space bar

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -8,18 +8,37 @@ public class BulletController : MonoBehaviour
     //private Rigidbody projectile;
 
     public GameObject projectile;
+    [SerializeField]
+    private bool shooting;
+    [SerializeField]
+    private int bulletsFired;
 
     private void Start()
     {
         //give the bullet a velocity when fired.
         //GetComponent<Rigidbody>().velocity = new Vector2(0, 20);
         //Debug.Log("A");
+        
     }
     private void Update()
     {
         //if spacebar is pressed
-        if (Input.GetKeyDown("Jump"))
+        if (Input.GetButton("Jump"))
         {
+            shooting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            shooting = false;
+        }
+        if (shooting == true)
+        {
+            ShootBullet();
+        }
+    }
+    private void ShootBullet()
+    {
+        
             //create new bullet gameobject and give position + speed.
             GameObject go = Instantiate(projectile, transform.position, transform.rotation);
             go.transform.parent = GameObject.Find("ShotCollector").transform;
@@ -27,9 +46,9 @@ public class BulletController : MonoBehaviour
             go.GetComponent<Rigidbody>().useGravity = false;
             go.GetComponent<Rigidbody>().freezeRotation = true;
             go.GetComponent<Rigidbody>().transform.localScale = new Vector3(0.04f, 0.05f, 1f);
-            go.GetComponent<Rigidbody>().velocity = new Vector2(0, 10);
-
-
-        }
+            go.GetComponent<Rigidbody>().velocity = new Vector2(0, 6);
+            bulletsFired++;
+        
     }
 }
+
