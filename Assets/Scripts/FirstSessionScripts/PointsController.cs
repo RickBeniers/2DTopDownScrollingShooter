b32680382e55307c0a6132ade83451c1406b0c1b
RickBeniers <Beniersrick@gmail.com>
2019-10-04 17:18:16 +0200

    Test01

--- Assets/Scripts/FirstSessionScripts/PointsController.cs
+++ Assets/Scripts/FirstSessionScripts/PointsController.cs
@@ -29,6 +29,7 @@ public class PointsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //djkykykdthsrsryrtdjygklfykdtsrtsysrdujdgcjfulfudrydujyhkygjdrdjfhlufjsrjdtjfyklfyli;olh,fyhtdryduyfjshdjfykyr
         XPcounter.text = "0";
         Goldcounter.text = "0";
         TimerText.text = TimeStart.ToString("F2");
